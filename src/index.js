6f0e0d1c0c2e3e1c675354f278179c7f96d83d9f
QuintyHH <razmocanu@yahoo.com>
2019-07-22 10:49:36 +0300

    added redux-form and hooked up userId to the authReducer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import Streemz from "./components/Streemz";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 render(
   <Provider store={store}>
