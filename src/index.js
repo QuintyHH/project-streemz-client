d0b234abdc813b5ad65febd272003740d7eb9877
QuintyHH <razmocanu@yahoo.com>
2019-07-22 06:51:58 +0300

    imported redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import Streemz from "./components/Streemz";
+import reducers from "./reducers";
 
-render(<Streemz />, document.querySelector("#root"));
+const store = createStore(reducers);
+
+render(
+  <Provider store={store}>
+    <Streemz />
+  </Provider>,
+  document.querySelector("#root")
+);
